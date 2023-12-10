4fbd12a1aacedef6f56dc4d0a589248a5413cbbd
SiYun Zou <siyunzou@siyuns-mbp.lan>
2023-12-10 12:24:06 -0500

    rpokewrapper

---
+++ R/devstuff.R
@@ -0,0 +1,49 @@
+library(devtools)
+library(usethis)
+library(desc)
+#devtools::install_github("r-lib/desc")
+
+
+# Remove default DESC
+unlink("rpokewrapper")
+# Create and clean desc
+my_desc <- description$new("!new")
+
+# Set your package name
+my_desc$set("rpokewrapper", "rpokewrapper")
+
+#Set your name
+my_desc$set("Authors@R", "person('Siyun', 'Zou', email = 'sz3351@nyu.edu', role = c('cre', 'aut'))")
+
+# Remove some author fields
+my_desc$del("Maintainer")
+
+# Set the version
+my_desc$set_version("0.0.0.9000")
+
+# The title of your package
+my_desc$set(Title = "My Pokemon API Wrapper")
+# The description of your package
+my_desc$set(Description = "This a Pokemon API Wrapper that assist with pulling some statistics from the Pokemon API.")
+# The urls
+my_desc$set("URL", "http://this")
+my_desc$set("BugReports", "http://that")
+# Save everything
+my_desc$write(file = "rpokewrapper")
+
+# If you want to use the MIT licence, code of conduct, and lifecycle badge
+use_mit_license()
+
+# Get the dependencies
+use_package("httr")
+use_package("jsonlite")
+use_package("curl")
+use_package("attempt")
+use_package("purrr")
+
+# Clean your description
+use_tidy_description()
+
+devtools::check()
+# Then build it with:
+devtools::build()
---
+++ R/pokemon_df.R
@@ -0,0 +1,19 @@
+#' First Generation Pokemon Data
+#'
+#' A subset of data from the PokeAPI
+#'
+#' @format ## `pokemon`
+#' A data frame with 151 rows and 7 columns:
+#' \describe{
+#'   \item{id}{Pokemon id}
+#'   \item{pokemon}{Pokemon's name}
+#'   \item{height}{Height of pokemon}
+#'   \item{weight}{Weight of pokemon}
+#'   \item{abilities}{Abilities of pokemon}
+#'   \item{type}{Type of pokemon}
+#'   \item{moves}{Pokemon's top 5 moves}
+#'   ...
+#' }
+#' @source <https://pokeapi.co/>
+"pokemon"
+
